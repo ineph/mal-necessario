9b6562d7c101bb3e2dd7dfabbbb9d628aaff4157
ineps <lcs999999999@hotmail.com>
2021-02-19 04:05:31 -0300

    menus

--- main.cpp
+++ main.cpp
@@ -1,28 +1,52 @@
-
 #define UNICODE
 #define TEXT
 #define DEFAULT_BUTTON_WIDTH 100
 #define DEFAULT_BUTTON_HEIGHT 100
 #include <Windows.h>
+#include <windows.h>
 #include <WinUser.h>
 
-
 HWND Window;
+HWND bCreateWall;
+//evento de comando
+void WMCommand(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam){
+    if (lParam == (LPARAM)bCreateWall)
+        MessageBox(Window, L"vc apertou bCreateWall", L"BOA!", MB_OK);
+    // else if (wParam == ID_FILE_EXIT) PostQuitMessage(0);
+}
 
-LRESULT CALLBACK WndowProc (HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+//gerenciador de mensagem e quase tudo que ocorre pois é o loop principal
+LRESULT CALLBACK WndowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
         case WM_DESTROY: PostQuitMessage(0); break;
+        case WM_CREATE: {
+            HMENU hMenu = CreateMenu();
+            HMENU hFile = CreateMenu();
+            HMENU hOptions = CreateMenu();
+
+            AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFile, L"File");
+            AppendMenu(hMenu, MF_POPUP, NULL, L"Edit");
+            AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hOptions, L"Options");
+
+            AppendMenu(hFile, MF_STRING, NULL, L"Exit");
+            AppendMenu(hFile, MF_STRING, NULL, L"1");
+            AppendMenu(hFile, MF_STRING, NULL, L"2");
+            SetMenu(hWnd, hMenu);
+        } break;
+        case WM_COMMAND: WMCommand(hWnd, msg, wParam, lParam); break;
     }
     return (DefWindowProc(hWnd, msg, wParam, lParam));
 }
 
-int WINAPI WinMain (
+//main
+int WINAPI WinMain(
 HINSTANCE hInstance,
 HINSTANCE hPrevious,
 LPSTR IpCmdString,
 int CmdShow){
 
-    const wchar_t CLASS_NAME[] = L"Map Editor";
+    const wchar_t CLASS_NAME[] = L"ME";
     WNDCLASS wc = {};
     MSG      msg;
 
@@ -53,8 +77,26 @@ int CmdShow){
         return(0);
     }
 
-    Window = CreateWindowEx(0, L"Map Editor", L"Map Editor", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        0, 0, 640, 480, NULL, NULL, hInstance, NULL);
+    Window = CreateWindowEx(
+        0,
+        L"ME",
+        L"Map Editor",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        0, 0, 640, 480,
+        NULL, NULL,
+        hInstance, NULL);
+    
+    bCreateWall = CreateWindow(
+        L"BUTTON",
+        L"Create Wall",
+        WS_CHILD | WS_VISIBLE,
+        0, 100,
+        DEFAULT_BUTTON_WIDTH,
+        DEFAULT_BUTTON_HEIGHT,
+        Window,
+        NULL,
+        hInstance,
+        NULL);
 
     if (Window == NULL){
         MessageBox(NULL, L"Erro: Falha ao criar janela", L"ERRO!", MB_OK);
